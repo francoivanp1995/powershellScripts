aafb3dba22bb51e2f72a53efa3f4431af49b0254
Franco Paco <77648668+francoivanp1995@users.noreply.github.com>
2021-03-15 19:12:15 -0300

    Add files via upload

---
+++ PS to remove sec group from admin group.ps1
@@ -0,0 +1,10 @@
+﻿$servers = Get-Content -Path C:\Users\**userfolder**\Desktop\VM-List.txt
+
+foreach ($server in $servers){
+    Write-Verbose "Working on server $server..." -Verbose
+    $vm = $server
+
+    $code = ([ADSI]"WinNT://$vm/Administrators").Remove("WinNT://DIR/**security group to remove**")
+    
+}
+
